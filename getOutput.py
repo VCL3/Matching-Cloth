60c65b7b495f079e8e91f6af178432d14ce429ff
Lydia <lydia.yuyantang@gmail.com>
2015-04-19 11:41:16 -0400

    five resulsts

--- getOutput.py
+++ getOutput.py
@@ -8,59 +8,77 @@ def getTarget(targetFile, imageFilenameRoot, inputFile, outputFile):
 	outfile = open(outputFile, "w")
 	infile = open(inputFile, "w")
 
+	tshirts = ["t-shirt", "poloshirt"]
 	bottoms = ["trousers", "trouser", "jeans", "pants", "sweatpants", "jogger", "chino"]
 	num_top = 0
 	num_bottom = 0
 	num_suit = 0
 	num_shorts = 0
+	num_tshirts = 0
 	i = 20
-	max_num = 26
+	max_num = 32
+	verbose = True
 	while 1:
 		isTop = True 
 
 		line = target.readline().split()
 		if len(line) == 0: break
-		if "Suit" in line[1]:
+		if "suit" in line[1].lower():
 			if num_suit >= max_num:
 				continue
-			outfile.write("1 0 0 0\n")
+			outfile.write("1 0 0 0 0\n")
 			isTop = False
-			#print "SUIT", line
-
+			if verbose:
+				print "SUIT", line
 			num_suit += 1
 
 		if "shorts" == line[1].lower():
 			if num_shorts >= max_num:
 				continue
-			outfile.write("0 0 1 0\n")
+			outfile.write("0 0 0 1 0\n")
+			isTop = False
 
 			num_shorts += 1
+			if verbose:
+				print "SHORTS", line
+
+
+		for t in tshirts:
+			if t == line[1].lower():
+				if num_tshirts >= max_num:
+					continue
+				outfile.write("0 1 0 0 0\n")
+				isTop = False
+				num_tshirts += 1
+				if verbose:
+					print "TSHIRT", line
 
 		for b in bottoms:
 			if b == line[1].lower():
 				if num_bottom >= max_num:
 					continue
-				outfile.write("0 0 0 1\n")
+				outfile.write("0 0 0 0 1\n")
 				#print "BOTTOM", line
 				isTop = False
-
 				num_bottom += 1
+				if verbose:
+					print "BOTTOM", line
 
 
 		if isTop:
 			if num_top >= max_num:
 				continue
-
-			outfile.write("0 1 0 0\n")
-			#print "TOP", line
+			outfile.write("0 0 1 0 0\n")
 			num_top += 1
+			if verbose:
+				print "TOP", line
+
 
 		image = cv2.imread(imageFilenameRoot + line[0]+".jpg")
 		# h = 144/2
 		# w = 108/2
-		h = 29
-		w = 23
-
+		h = 144
+		w = 108
 		smallImg = numpy.empty((h,w), 'uint8')
 		image = cv2.resize(image, (w,h), smallImg, 0, 0, cv2.INTER_LANCZOS4)
 		smallImg = smallImg.astype(float)
@@ -83,7 +101,7 @@ def getTarget(targetFile, imageFilenameRoot, inputFile, outputFile):
 				
 		infile.write("\n")
 
-	print "All done", "top:", num_top, "bottoms:", num_bottom, "suits:", num_suit, "shorts", num_shorts	
+	print "All done", "top:", num_top, "tshirts", num_tshirts, "bottoms:", num_bottom, "suits:", num_suit, "shorts", num_shorts	
 
 		# print smallImg.shape
 
